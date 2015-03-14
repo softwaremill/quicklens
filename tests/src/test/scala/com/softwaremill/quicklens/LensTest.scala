2415de0c2faa4b538220a3431846c775a7e65d08
adamw <adam@warski.org>
2015-03-14 12:39:30 +0100

    Lens test

---
+++ tests/src/test/scala/com/softwaremill/quicklens/LensTest.scala
@@ -0,0 +1,19 @@
+package com.softwaremill.quicklens
+
+import com.softwaremill.quicklens.TestData._
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class LensTest extends FlatSpec with ShouldMatchers {
+  it should "create reusable lens of the given type" in {
+    val lens = modify(_: A1)(_.a2.a3.a4.a5.name)
+
+    lens(a1).using(duplicate) should be (a1dup)
+  }
+
+  it should "compose lens" in {
+    val lens_a1_a3 = modify(_: A1)(_.a2.a3)
+    val lens_a3_name = modify(_: A3)(_.a4.a5.name)
+
+    (lens_a1_a3 andThenModify lens_a3_name)(a1).using(duplicate) should be (a1dup)
+  }
+}
