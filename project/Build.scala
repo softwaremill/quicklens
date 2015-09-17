5a231cf9d1cfce257b1d53dabad52de7c8c807d6
adamw <adam@warski.org>
2015-09-17 12:45:37 +0200

    1.4.1 release

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import sbt._
 object BuildSettings {
   val buildSettings = Defaults.coreDefaultSettings ++ Seq (
     organization  := "com.softwaremill.quicklens",
-    version       := "1.4.0",
+    version       := "1.4.1",
     scalaVersion  := "2.11.6",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
