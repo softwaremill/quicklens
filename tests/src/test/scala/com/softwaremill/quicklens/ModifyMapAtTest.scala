335b90b27119d06dabeec1aaeb6157d3e39273a3
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-02-05 10:27:24 +0100

    Removed forgotten line copied from list tests.

--- tests/src/test/scala/com/softwaremill/quicklens/ModifyMapAtTest.scala
+++ tests/src/test/scala/com/softwaremill/quicklens/ModifyMapAtTest.scala
@@ -7,7 +7,6 @@ class ModifyMapAtTest extends FlatSpec with ShouldMatchers {
 
   it should "modify a non-nested map with case class item" in {
     modify(m1)(_.at("K1").a5.name).using(duplicate) should be(m1dup)
-    //modify(l1)(_.at(2)).using(a3 => modify(a3)(_.a4.a5.name).using(duplicate)) should be(l1at2dup)
   }
 
   it should "modify a nested map using at" in {
