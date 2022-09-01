7ce707a0a22e2d2d0ab9ff9ee28cfd207924cae0
Kacper Korban <kacper.f.korban@gmail.com>
2022-09-01 18:55:07 +0200

    Add another self type test

--- quicklens/src/test/scala/com/softwaremill/quicklens/ModifySelfThisTest.scala
+++ quicklens/src/test/scala/com/softwaremill/quicklens/ModifySelfThisTest.scala
@@ -7,15 +7,32 @@ import ModifySelfThisTest._
 
 object ModifySelfThisTest {
 
-  case class State(x: Int) {self =>
+  case class State(x: Int) { self =>
 
     def mod: State = this.modify(_.x).setTo(1)
   }
 
+  trait A {
+    def a: Unit
+  }
+
+  case class State1(x: Int) extends A { self: A =>
+
+    def mod: State1 = this.modify(_.x).setTo(1)
+
+    def a: Unit = ()
+  }
 }
 
 class ModifySelfThisTest extends AnyFlatSpec with Matchers {
   it should "modify an object even in presence of self alias" in {
+    val s = State(0)
+    val modified = s.mod
+
+    modified.x shouldBe 1
+  }
+
+  it should "modify an object even in presence of self type" in {
     val s = State(0)
     val modified = s.mod
 
